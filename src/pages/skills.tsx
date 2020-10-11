0c73b9b6a85552af3bc9c7ae7830ddd93f7b388d
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-11 16:05:56 +0330

    page title added

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Hero from "components/Skills/Hero";
 
 const Skills: React.FC = () => {
   return (
     <div>
+      <Head>
+        <title>Skills and Abilities | Hossein Ahmadi</title>
+      </Head>
       <Hero />
     </div>
   );
