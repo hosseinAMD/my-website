bc2de251c8303b93102b7a436b72934eb5f97b0d
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-11 17:46:04 +0330

    skills hero design changed

--- src/components/Skills/Hero.tsx
+++ src/components/Skills/Hero.tsx
@@ -1,19 +1,36 @@
 const Hero: React.FC = () => {
   return (
-    <div className="py-10">
+    <div className="py-10 bg-yellow-400">
       <div className="container mx-auto">
-        <div className="w-1/2 mx-auto grid grid-cols-3 gap-8 items-center">
-          <img className="w-32 mx-auto" src="/img/react.png" alt="React Logo" />
-          <img
-            className="w-32 mx-auto"
-            src="/img/web.png"
-            alt="Web Techs Logo"
-          />
-          <img
-            className="w-32 mx-auto"
-            src="/img/responsive.png"
-            alt="Responsive Design Logo"
-          />
+        <div className="w-2/3 mx-auto grid grid-cols-2 gap-8 items-center">
+          <div>
+            <p className="text-3xl text-gray-800 font-bold">
+              What You Can{" "}
+              <span className="text-5xl text-blue-600">EXPECT</span>
+            </p>
+            <p className="text-3xl text-gray-800 font-bold">
+              From <span className="text-5xl text-blue-600">Me</span>
+            </p>
+          </div>
+          <div className="flex flex-col space-y-4">
+            <img
+              className="w-32 mx-auto"
+              src="/img/web.png"
+              alt="Web Techs Logo"
+            />
+            <div className="grid grid-cols-2">
+              <img
+                className="w-32 mx-auto"
+                src="/img/react.png"
+                alt="React Logo"
+              />
+              <img
+                className="w-32 mx-auto"
+                src="/img/responsive.png"
+                alt="Responsive Design Logo"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
