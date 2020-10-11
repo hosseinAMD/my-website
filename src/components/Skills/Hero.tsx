ca7ea8d64b15fcfe967903ae8ddd70504b1ccec0
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-11 17:55:13 +0330

    skills hero responsive design done

--- src/components/Skills/Hero.tsx
+++ src/components/Skills/Hero.tsx
@@ -1,31 +1,31 @@
 const Hero: React.FC = () => {
   return (
-    <div className="py-10 bg-hue">
+    <div className="py-6 md:py-10 bg-hue">
       <div className="container mx-auto">
-        <div className="w-2/3 mx-auto grid grid-cols-2 gap-8 items-center">
+        <div className="mx-4 md:w-2/3 md:mx-auto md:grid md:grid-cols-2 md:gap-8 md:items-center">
           <div>
-            <p className="text-3xl text-gray-800 font-bold">
+            <p className="text-3xl text-gray-800 font-bold text-center md:text-left">
               What You Can{" "}
               <span className="text-5xl text-blue-600">EXPECT</span>
             </p>
-            <p className="text-3xl text-gray-800 font-bold">
+            <p className="text-3xl text-gray-800 font-bold text-center md:text-left">
               From <span className="text-5xl text-blue-600">Me</span>
             </p>
           </div>
-          <div className="flex flex-col space-y-4">
+          <div className="flex flex-row mt-4 md:mt-0 md:flex-col space-x-4 md:space-y-4">
             <img
-              className="w-32 mx-auto"
+              className="w-20 md:w-32 mx-auto"
               src="/img/web.png"
               alt="Web Techs Logo"
             />
-            <div className="grid grid-cols-2">
+            <div className="grid grid-cols-2 gap-4">
               <img
-                className="w-32 mx-auto"
+                className="w-20 md:w-32 mx-auto"
                 src="/img/react.png"
                 alt="React Logo"
               />
               <img
-                className="w-32 mx-auto"
+                className="w-20 md:w-32 mx-auto"
                 src="/img/responsive.png"
                 alt="Responsive Design Logo"
               />
