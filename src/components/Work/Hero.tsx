5972ed490d828d4f86e59cb0c53204b50e41656a
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-12 11:02:58 +0330

    work experiences page created

---
+++ src/components/Work/Hero.tsx
@@ -0,0 +1,5 @@
+const Hero: React.FC = () => {
+  return <div>Work Hero</div>;
+};
+
+export default Hero;
---
+++ src/pages/work-experiences.tsx
@@ -0,0 +1,15 @@
+import Hero from "components/Work/Hero";
+import Head from "next/head";
+
+const Works: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Work Experiences | Hossein Ahmadi</title>
+      </Head>
+      <Hero />
+    </div>
+  );
+};
+
+export default Works;
