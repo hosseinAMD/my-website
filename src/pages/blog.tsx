1f103b746ef668477ca6aadece7e3adde7282382
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-26 10:41:17 +0330

    blog page created

---
+++ src/pages/blog.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Blog: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Blogs | Hossein Ahmadi</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Blog;
