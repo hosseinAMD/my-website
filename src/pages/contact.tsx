f20659f21a73c8b204ccadf18f1d180a0253902e
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-28 11:08:35 +0330

    contact page created

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Contact: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Contact | Hossein Ahmadi</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Contact;
