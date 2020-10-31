7cc4aa07a6ffb6a3a38d01cc1021297ee95d9306
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-31 09:22:32 +0330

    contact linkes done

--- src/components/Contact/ContactBadges.tsx
+++ src/components/Contact/ContactBadges.tsx
@@ -1,42 +1,48 @@
 const ContactBadges: React.FC = () => {
   return (
     <div className="grid grid-cols-2 gap-12 md:grid-cols-6">
-      <a href="">
+      <a href="mailto:hossein98.ahmadi@gmail.com" target="_blank">
         <img
           src="https://img.shields.io/badge/gmail-D14836?&style=for-the-badge&logo=gmail&logoColor=white"
           className="w-full rounded-lg h-full"
           alt="gmail"
         />
       </a>
-      <a href="">
+      <a href="https://twitter.com/Hossein98Ahmadi" target="_blank">
         <img
           src="https://img.shields.io/badge/twitter-%231DA1F2.svg?&style=for-the-badge&logo=twitter&logoColor=white"
           className="w-full rounded-lg h-full"
           alt="twitter"
         />
       </a>
-      <a href="">
+      <a
+        href="https://www.linkedin.com/in/hossein-ahmadi-46690a116/"
+        target="_blank"
+      >
         <img
           src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white"
           className="w-full rounded-lg h-full"
           alt="linkedin"
         />
       </a>
-      <a href="">
+      <a href="https://github.com/hosseinAMD" target="_blank">
         <img
           src="https://img.shields.io/badge/github-%23100000.svg?&style=for-the-badge&logo=github&logoColor=white "
           className="w-full rounded-lg h-full"
           alt="github"
         />
       </a>
-      <a href="">
+      <a
+        href="https://stackoverflow.com/users/11752942/hossein-ahmadi"
+        target="_blank"
+      >
         <img
           src="https://img.shields.io/badge/stack%20overflow-FE7A16?logo=stack-overflow&logoColor=white&style=for-the-badge "
           className="w-full rounded-lg h-full"
           alt="stackoverflow"
         />
       </a>
-      <a href="">
+      <a href="https://medium.com/@hossein98.ahmadi" target="_blank">
         <img
           src="https://img.shields.io/badge/medium-%2312100E.svg?&style=for-the-badge&logo=medium&logoColor=white"
           className="w-full rounded-lg h-full"
